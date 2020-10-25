a41487f044e528c5d890ecdf9588befcd64aae28
Erol Kahraman <69480343+kahramaneron@users.noreply.github.com>
2020-10-26 00:11:54 +0300

    Update question_7.py

--- question_7.py
+++ question_7.py
@@ -1,7 +1,12 @@
 
 First_digit + Second_digit is equal to Last_digit
 
+count = 0
+
 for i in range (100,1000):
     if int(str(i)[0]) + int(str(i)[1]) == int(str(i)[2]):
         print(i)
+        
+print(f"{count} total numbers.")
+
     
