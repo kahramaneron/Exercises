1c31fdc8b4127a80d3aa0428c80d7f24ae77d648
Erol Kahraman <69480343+kahramaneron@users.noreply.github.com>
2020-10-25 22:30:12 +0300

    Add files via upload

---
+++ hw_esra_hoca.py
@@ -0,0 +1,15 @@
+??? kullanicidan bir sayi alsin, tek mi cift mi onu bastirsin ???
+
+
+number = int(input('enter a number: '))
+
+if number % 2 == 0:
+    print('cift sayi')
+
+else:
+    print('tek sayi')
+
+
+             
+
+
