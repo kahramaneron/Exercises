3489f0288de7917fd5b112271f9f04ea6c8cf5d5
Erol Kahraman <69480343+kahramaneron@users.noreply.github.com>
2020-11-02 22:15:09 +0300

    Add files via upload

---
+++ question_13.py
@@ -0,0 +1,7 @@
+
+for num1 in range(10,100):
+    num1=str(num1)
+    for num2 in range(10,100):
+        num2=str(num2)
+        if int(num1+num2)==11*(int(num1)+int(num2)):
+            print(f'first number is: {num1} and second number is: {num2}')
